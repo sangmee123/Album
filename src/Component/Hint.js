73de01ad858f54d96240711abfd50a0dd36a5118
sangmee123 <oeanb12@gmail.com>
2023-10-24 20:42:39 +0900

    Hint.css => CSS in JS(styled-components)

--- src/Component/Hint.js
+++ src/Component/Hint.js
@@ -1,7 +1,46 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
-import '../style/Hint.css';
+const CenterBox = styled.div`
+    text-align: center;
+`;
+
+const Font = styled.span`
+    font-size: 60px;
+    font-family: 'Sunflower', 'sans-serif';
+    &:hover {
+        color:rgb(123, 168, 40);
+    }
+
+    ${props => 
+      props.value === 'pw' &&
+      css`
+        display: block;
+        margin-top: 30px;  
+    `};
+`;
+
+const Li = styled.li`
+    color: black;
+    font-weight: lighter;
+    font-size: 30px;
+    list-style: none;
+    margin-top: 20px;
+    margin-bottom: 30px;    
+`;
+
+const Back = styled.button`
+    width: 18%;
+    height: 35px;
+    margin-top: 10%;
+    border: 1px solid gray;
+    border-radius: 15px;   
+    opacity: 0.85;
+    &:hover {
+        background-color: #d8db31;
+    }
+`;
 
 const Hint = () => {
     const [visibleId, setVisibleId] = useState(false);
@@ -9,27 +48,24 @@ const Hint = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="center">            
-            <span 
-                className="clickID" 
-                onClick={() => setVisibleId(!visibleId)}
-            >
+        <CenterBox>            
+            <Font onClick={() => setVisibleId(!visibleId)}>
                 당신의 ID는?
-            </span>
-            {visibleId && <li>귀하의 <b>성함</b>을 입력하시면 됩니다.</li>}
+            </Font>
+            {visibleId && <Li>귀하의 <b>성함</b>을 입력하시면 됩니다.</Li>}
             
-            <span 
-                className="clickPASSWORD" 
+            <Font 
+                value="pw" 
                 onClick={() => setVisiblePw(!visiblePw)}
             >
                 당신의 PASSWORD는?
-            </span>
-            {visiblePw && <li>귀하의 <b>생년월일</b>을 입력하세요.</li>}
+            </Font>
+            {visiblePw && <Li>귀하의 <b>생년월일</b>을 입력하세요.</Li>}
             
             <div>
-                <button className="back" onClick={() => navigate(-1)}>로그인 하기</button>
+                <Back onClick={() => navigate(-1)}>로그인 하기</Back>
             </div>
-        </div>
+        </CenterBox>
     )
 }
 
